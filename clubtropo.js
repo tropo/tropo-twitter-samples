b378b3fbfed40a1a6df30aa541219769570dd9da
Dan York <dyork@Lodestar2.com>
2010-11-30 10:25:44 -0500

    Adding Club Tropo message app

---
+++ clubtropo.js
@@ -0,0 +1,8 @@
+message = currentCall.initialText;
+if(message == "Tropo Rocks!"){
+    say("You just learned how to make a Twitter bot with @Tropo for free. Welcome to the club.");
+}
+else {
+    say ("Say what? Try it again - send us a tweet with the correct password to get on the list.");
+}
+
