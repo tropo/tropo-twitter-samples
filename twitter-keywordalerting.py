08774e47d23be09d6110e19ff49e1bf2074f9e0a
Dan York <dyork@Lodestar2.com>
2010-12-01 10:45:44 -0500

    Added more comments

--- twitter-keywordalerting.py
+++ twitter-keywordalerting.py
@@ -5,10 +5,22 @@
 
 text = currentCall.initialText.lower()
 
+# If someone tweets either a reply or a mention that includes the word "fail"
+# send a text message to a cell phone to that the recipient can check out the
+# tweet
+
 if text.find("fail") > -1:
     message("Whoa! " + currentCall.callerID + " is tweeting a fail",   {"to":"tel:+14075551212", "network":"SMS"})
     
-elif text.find("@tropohelp") == -1:   #Is it a reply?
+# Is the message a "reply", i.e. a message directly to the Twitter account?
+# If so, check the message for different keywords and take actions.
+
+# (The reason for not sending messages out in reply to a "mention" is that it
+# may be harder to guess exactly what the person is looking for based purely on a 
+# keyword.  With a message sent to the account (a "reply") you have a better 
+# chance of replying with accurate info.
+
+elif text.find("@tropohelp") == -1:  
 
     if text.find("help") > -1:
         say("Tropo lets you build apps that interact via voice, SMS, IM and Twitter. See http://www.tropo.com/ for more.")
